2d7cef9a3070fcfb37962ff0da658da021762643
Dejan Spasić <dejan.spasic@bitweise.biz>
2022-02-04 21:02:45 +0100

    Playing Arround With BaseSubscriber
    There is an additional subscribe method that is more generic and takes a
    full-blown Subscriber rather than composing one out of lambdas. In order to
    help with writing such a Subscriber, we provide an extendable class called
    BaseSubscriber.

--- jyard-reactor/src/main/java/biz/bitweise/jyard/reactor/App.java
+++ jyard-reactor/src/main/java/biz/bitweise/jyard/reactor/App.java
@@ -1,12 +1,16 @@
 package biz.bitweise.jyard.reactor;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
 
 public class App {
 
   public static void main(String[] args) {
     subscribeHandleSubscriber();
     onErrorContinue();
+    sampleSubscriber();
   }
 
   /**
@@ -43,5 +47,45 @@ public class App {
             (throwable, object) ->
                 System.err.printf("This object %s has following issues: %s", object, throwable))
         .subscribe(System.out::println);
+  }
+
+  /**
+   *  An Alternative to Lambdas: BaseSubscriber
+   *
+   *  There is an additional subscribe method that is more generic and takes a full-blown
+   *  Subscriber rather than composing one out of lambdas. In order to help with writing such
+   *  a Subscriber, we provide an extendable class called BaseSubscriber.
+   */
+  private static void sampleSubscriber() {
+    SampleSubscriber<Integer> subscriber = new SampleSubscriber<>();
+    Flux.range(0, 9).subscribe(subscriber);
+  }
+
+  private static class SampleSubscriber<T> extends BaseSubscriber<T> {
+    @Override public void hookOnSubscribe(final Subscription sub) {
+      System.out.println(this.getClass().getSimpleName() + " subscribed");
+      request(1);
+    }
+
+    @Override public void hookOnNext(final T value) {
+      System.out.printf("Value %s received%n", value);
+      request(1);
+    }
+
+    @Override public void hookOnComplete() {
+      System.out.println("Complete.");
+    }
+
+    @Override public void hookFinally(final SignalType st) {
+      System.out.printf("Finally done %s%n", st);
+    }
+
+    @Override public void hookOnCancel() {
+      System.out.println("Canceled, but why mate?!");
+    }
+
+    @Override public void hookOnError(final Throwable t) {
+      System.err.printf("Don't care: %s", t);
+    }
   }
 }
