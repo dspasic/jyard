7e41487be5b825426bcfded0a6a7af0cb16d52b4
Dejan Spasic <dejan.spasic@bitweise.biz>
2025-02-09 23:03:27 +0100

    update

--- jyard-performance/src/main/java/biz/bitweise/jyard/performance/OsSchedulerOverload.java
+++ jyard-performance/src/main/java/biz/bitweise/jyard/performance/OsSchedulerOverload.java
@@ -1,5 +1,12 @@
 package biz.bitweise.jyard.performance;
 
+/**
+ * One of the easiest ways to see the action and behavior of a scheduler is to try to observe the
+ * overhead imposed by the OS to achieve scheduling. The following code executes 2,000 separate 2 ms
+ * sleeps. Each of these sleeps will involve the thread being sent to the back of the run queue, and
+ * having to wait for a new time quantum. So, the total elapsed time of the code gives us some idea
+ * of the overhead of scheduling for a typical process
+ */
 public class OsSchedulerOverload {
 
   public void overloadOsScheduler() {
@@ -12,7 +19,6 @@ public class OsSchedulerOverload {
       }
     }
     long end = System.currentTimeMillis();
-    System.out.printf("Millis elapsed %f", (end - start)/4000.0);
+    System.out.printf("Millis elapsed %f", (end - start) / 4000.0);
   }
-
 }
